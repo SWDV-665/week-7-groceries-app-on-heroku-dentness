78d5a590869e6c59d6a2e471c3dc2e40bccf36b1
Joe Dent <14243877+dentness@users.noreply.github.com>
2019-07-16 22:30:07 -0500

    Week 3 exercise

--- src/app/grocerylist/grocerylist.page.ts
+++ src/app/grocerylist/grocerylist.page.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlertController, ToastController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-grocieries',
-  templateUrl: 'grocerylist.page.html',
-  styleUrls: ['grocerylist.page.scss']
+    selector: 'app-grocieries',
+    templateUrl: 'grocerylist.page.html',
+    styleUrls: ['grocerylist.page.scss']
 })
 export class GrocerylistPage {
 
-  constructor() {}
+    title = 'Grocery List';
+    items = [
+        {name: 'Milk', qty: 2},
+        {name: 'Bread', qty: 1},
+        {name: 'Eggs', qty: 3},
+    ];
+
+    constructor(private toastController: ToastController, public alertController: AlertController) {
+    }
+
+
+    addItem(itemName, quantity) {
+        this.items.push({qty: quantity, name: itemName});
+    }
+
+    removeItem(item, index) {
+        console.log('Removing ', item);
+        this.items.splice(index, 1);
+        this.presentToast('Removing item: ' + item.name + ' .... ');
+    }
+
+    async presentToast(msg) {
+        const toast = await this.toastController.create({
+            message: msg,
+            duration: 2000,
+            position: 'top'
+        });
+        toast.present();
+    }
+
+    async presentAddGroceryItemPrompt() {
+        const alert = await this.alertController.create({
+            header: 'Add Grocery Item!',
+            inputs: [
+                {
+                    name: 'itemName',
+                    type: 'text',
+                    placeholder: 'Name'
+                },
+                {
+                    name: 'quantity',
+                    type: 'number',
+                    placeholder: 'Quantity',
+                    min: 0
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: () => {
+                        console.log('Confirm Cancel');
+                    }
+                }, {
+                    text: 'Ok',
+                    handler: (data) => {
+                        console.log('Confirm Ok');
+                        this.addItem(data.itemName, data.quantity);
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
 
 }
